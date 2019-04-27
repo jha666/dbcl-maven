5602c72246688ac9c49d533a2c1615e8d4be2887
Johan Andersson <jha@independent.se>
2019-04-27 09:18:27 +0200

    Support for other dataabses

--- src/main/java/se/independent/dbclassloader/maven/AbstractDbCLMojo.java
+++ src/main/java/se/independent/dbclassloader/maven/AbstractDbCLMojo.java
@@ -9,7 +9,9 @@ import java.sql.DriverPropertyInfo;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
 import java.util.Properties;
+import java.util.Scanner;
 import java.util.logging.Logger;
+import java.util.regex.MatchResult;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -17,6 +19,10 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 import se.independent.dbclassloader.DbClassLoaderManager;
 import se.independent.dbclassloader.JDBCDbClassLoaderManager;
+import se.independent.dbclassloader.JedisDbClassLoaderManager;
+import se.independent.dbclassloader.MongoDBDbClassLoaderManager;
+import se.independent.dbclassloader.Neo4jDbClassLoaderManager;
+import se.independent.dbclassloader.SQLiteDbClassLoaderManager;
 
 public abstract class AbstractDbCLMojo extends AbstractMojo {
 	
@@ -58,7 +64,48 @@ public abstract class AbstractDbCLMojo extends AbstractMojo {
 		        p.setProperty("user", dbUser);
 		        p.setProperty("password", dbPasswd);
 		        
-		        _manager = new JDBCDbClassLoaderManager();
+		        
+				String[] URL = jdbcURL.split(":");
+			     
+			    switch (URL[0]) {
+			    case "jdbc":   // jdbc:<subprotocol>:<subname>
+			    	 String subprotocol = "";
+			    	 String subname = "";
+		   		 
+			    	 try (Scanner scanner = new Scanner(jdbcURL)) {
+			    		 scanner.findInLine("jdbc:(\\w+):(.*)");
+			    		 MatchResult result = scanner.match();
+			    		 subprotocol = result.group(1);
+			    		 subname = result.group(2);        	     
+			    	 }
+		   		    		 
+			    	 switch (subprotocol) {
+			    	 case "sqlite":
+			    		 _manager = new SQLiteDbClassLoaderManager();
+			    		 break;
+			    		 
+			    	 default:
+			    		 _manager = new JDBCDbClassLoaderManager();
+			    		 break;
+			    	 }
+			    	 break;
+			    	 
+			     case "redis":
+			    	 _manager = new JedisDbClassLoaderManager();
+			    	 break;
+			    	 
+			     case "mongodb":
+			    	 _manager = new MongoDBDbClassLoaderManager();
+			    	 break;
+			    	 
+			     case "bolt":
+			    	 _manager = new Neo4jDbClassLoaderManager();
+			    	 break;
+			    	 
+			     default:
+			    	 break;
+			     }
+		        
 		    	getLog().info("- execute() jdbcURL=" + jdbcURL);
 		        _manager.connect(jdbcURL, p);
 		        _manager.prepare();
