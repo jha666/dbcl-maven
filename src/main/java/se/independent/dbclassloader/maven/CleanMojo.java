2a56b2ac2028b8087ad212abab8067a9d2829988
Johan Andersson <jha@independent.se>
2019-04-27 09:16:24 +0200

    clean up of jar

--- src/main/java/se/independent/dbclassloader/maven/CleanMojo.java
+++ src/main/java/se/independent/dbclassloader/maven/CleanMojo.java
@@ -1,15 +1,11 @@
 package se.independent.dbclassloader.maven;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.jar.JarFile;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-
-import se.independent.dbclassloader.DbClassLoaderManager;
 
 /**
  * Goal which installs the artifact into a DbCL database
@@ -40,7 +36,7 @@ public class CleanMojo
 			_manager.removetWar(pathElement);
 		} 
 		else if ("jar".equalsIgnoreCase(packaging)) {
-			_manager.deletePathElement(pathElement, null);
+			_manager.deleteJar(pathElement);
 		} else {
 			getLog().warn("Dbcl_clean: ignoring " + pathElement);
 		}
